4fbc0d8a45da1265c588ec876f52a0e161d98566
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2025-09-08 17:02:49 +0200

    Refactor to remove pre-build.py (add to bld.bat)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,13 +5,10 @@ if %ARCH%==32 (
 )
 
 set EPICS_BASE=%PREFIX%\epics
+echo INSTALL_LOCATION = %EPICS_BASE:\=/%> configure\CONFIG_SITE.local
 
 REM SCRIPTS causes failure of GNU make
 set SCRIPTS=
-
-REM set up build
-copy %RECIPE_DIR%\pre-build.py %SRC_DIR%
-python pre-build.py
 
 echo Building at %CD%
 make -j %CPU_COUNT%
@@ -22,3 +19,20 @@ if errorlevel 1 (
 
 xcopy "%EPICS_BASE%\bin\%EPICS_HOST_ARCH%\*.dll" "%PREFIX%\Library\bin\" /Y
 xcopy "%EPICS_BASE%\lib\%EPICS_HOST_ARCH%\*.lib" "%PREFIX%\Library\lib\" /Y
+
+if not exist "%PREFIX%\etc\conda\activate.d" mkdir "%PREFIX%\etc\conda\activate.d"
+if not exist "%PREFIX%\etc\conda\deactivate.d" mkdir "%PREFIX%\etc\conda\deactivate.d"
+
+echo @echo off> "%PREFIX%\etc\conda\activate.d\epics-base_activate.bat"
+echo set "EPICS_BASE=%%CONDA_PREFIX%%\epics">> "%PREFIX%\etc\conda\activate.d\epics-base_activate.bat"
+echo set "EPICS_HOST_ARCH=%EPICS_HOST_ARCH%">> "%PREFIX%\etc\conda\activate.d\epics-base_activate.bat"
+echo set "EPICS_BASE_VERSION=%PKG_VERSION%">> "%PREFIX%\etc\conda\activate.d\epics-base_activate.bat"
+echo set "EPICS_BASE_HOST_BIN=%%CONDA_PREFIX%%\epics\bin\%EPICS_HOST_ARCH%">> "%PREFIX%\etc\conda\activate.d\epics-base_activate.bat"
+echo set "PATH=%%EPICS_BASE_HOST_BIN%%;%%PATH%%">> "%PREFIX%\etc\conda\activate.d\epics-base_activate.bat"
+
+echo @echo off> "%PREFIX%\etc\conda\deactivate.d\epics-base_deactivate.bat"
+echo set "PATH=%%PATH:%%EPICS_BASE_HOST_BIN%%;=%%">> "%PREFIX%\etc\conda\deactivate.d\epics-base_deactivate.bat"
+echo set "EPICS_BASE=">> "%PREFIX%\etc\conda\deactivate.d\epics-base_deactivate.bat"
+echo set "EPICS_HOST_ARCH=">> "%PREFIX%\etc\conda\deactivate.d\epics-base_deactivate.bat"
+echo set "EPICS_BASE_HOST_BIN=">> "%PREFIX%\etc\conda\deactivate.d\epics-base_deactivate.bat"
+echo set "EPICS_BASE_VERSION=">> "%PREFIX%\etc\conda\deactivate.d\epics-base_deactivate.bat"
